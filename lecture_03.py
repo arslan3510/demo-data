1de5c5c3b69b930dd535214a1db8f5766b607c75
arslan3510 <jut770109@gmail.com>
2024-09-12 10:43:45 +0500

    practice-data

---
+++ lecture_03.py
@@ -0,0 +1,17 @@
+# import matplotlib.pyplot as plt
+# import sys
+# x =[1,2,3,4,6,7]
+# y =[2,3,4,5,6,7]
+
+# plt.bar(x,y)
+# plt.show()
+
+# plt.savefig("ouputput.png")
+# sys.stdout.flush()
+
+# import seaborn as sp
+# import matplotlib.pyplot as plt
+
+# sp.kdeplot([1,2,3,4,5,6,7,8])
+# plt.savefig("out1.jpg")
+# plt.show()
